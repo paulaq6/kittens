e7f859626fd65bbe55ea7367e97a12801ed388ca
paolcia <paulina.m.kedziora@gmail.com>
2021-08-30 18:45:55 +0200

    fix: added exclamation mark

--- app/Http/Controllers/KittensController.php
+++ app/Http/Controllers/KittensController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class KittensController extends Controller
 {
     public function index () {
-        echo 'Paolciowy przycisk';
+        echo 'Paolciowy przycisk!';
     }
 }
